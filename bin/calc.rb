e6df0cae43e5339235d01a1993d06d2e6578d7fd
James Smith <james@floppy.org.uk>
2012-09-19 16:06:08 +0300

    Add generic calculation runner script

---
+++ bin/calc.rb
@@ -0,0 +1,18 @@
+require './lib/calc_json.rb'
+
+# Run like:
+# ruby bin/calc.rb examples/settling_velocity particle_density=2600.0 fluid_density=999.7 radius=0.001 viscosity=1.307
+
+schema_path = ARGV[0];
+
+inputs = Hash[ARGV.slice(1..-1).map{|x| x.split('=') }]
+
+model       = CalcJSON::Model.load_from_schema(schema_path)
+calculation = model.calculation(inputs)
+
+calculation.outputs.each do |result|
+  puts "#{result.definition.name} = #{result.value} #{result.unit.symbol}"
+end
+
+
+
