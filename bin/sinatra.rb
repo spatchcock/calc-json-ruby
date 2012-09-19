6c3f0e1d77ca6a4c4e06a324f4682cecd9918909
James Smith <james@floppy.org.uk>
2012-09-19 16:41:38 +0300

    Add sample sinatra web service

---
+++ bin/sinatra.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require './lib/calc_json.rb'
+
+# Request:
+# http://localhost:4567/settling_velocity?particle_density=2600.0&fluid_density=999.7&radius=0.001&viscosity=1.307
+
+get '/models/:model' do
+  schema_path = 'examples/' + params[:model]
+  
+  inputs = params.select{ |k,v| !["splat", "captures"].include?(k) && !params["captures"].include?(v)}
+  puts inputs.inspect
+  
+  model       = CalcJSON::Model.load_from_schema(schema_path)
+  calculation = model.calculation(inputs)
+
+  results = calculation.outputs.map do |result|
+    [result.definition.label, "#{result.value} #{result.unit.symbol}"]
+  end
+
+  Hash[results].to_json
+  
+end
+
+
+
+
+
+
+
+
