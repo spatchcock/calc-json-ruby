46a9f15f796679771e82ef85a496032062206e6d
James Smith <james@floppy.org.uk>
2012-09-21 01:23:44 +0300

    Add example

--- bin/calc.rb
+++ bin/calc.rb
@@ -2,6 +2,7 @@ require './lib/calc_json.rb'
 
 # Run like:
 # ruby bin/calc.rb examples/settling_velocity particle_density=2600.0 fluid_density=999.7 radius=0.001 viscosity=1.307
+# ruby bin/calc.rb examples/gravitational_force planetary_body=Pluto mass=10
 
 schema_path = ARGV[0];
 
